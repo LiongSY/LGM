7e67540c41c11fc8980498b1649fb17ee082ab92
LiongSY <longpang0213@gmail.com>
2023-10-13 01:20:12 +0800

    initial commit

---
+++ database/migrations/2023_10_12_170637_create_staff_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id('staffID');
+            $table->dateTime('lastSeen');
+            $table->string('status');
+            $table->foreignId('userID')->constrained('users', 'userID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staff');
+    }
+};
---
+++ database/migrations/2023_10_12_170834_create_beneficiary_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('beneficiary', function (Blueprint $table) {
+            $table->id('benID');
+            $table->string('benTitle');
+            $table->string('benName');
+            $table->string('benIC');
+            $table->string('benRelationship');
+            $table->string('benContact');
+            $table->string('benAddress');
+            
+            // Optional foreign key relationship with Customers table
+            $table->unsignedBigInteger('customerID')->nullable();
+            $table->foreign('customerID')->references('customerID')->on('customers')->onDelete('set null');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('beneficiary');
+    }
+};
