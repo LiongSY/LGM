bc147997f5f8cd5bf54827e3413e9f40d21c0a4c
LiongSY <longpang0213@gmail.com>
2023-10-26 01:48:30 +0800

    added multi-form

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -1,59 +1,140 @@
 @extends('layouts.app', ['class' => '', 'elementActive' => 'map'])
 
 @section('content')
-<div class="content"> <div class="row"> <div class="col-md-8"> <div class="card"> <div class="card-header"> Packages Management 
+<div class="content"> 
 
-</div>
+<div id="step1">
+    <div class="row"> 
+        <div class="col-md-8"> 
+            <div class="card"> <div class="card-header"> Itineray Management</div>
 
-<div class="card-body">
-    <!-- Form for Packages Management -->
-    <form action="" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div>
-            <div class="firstRow">
-                <div class="form-row">
-                    <div class="form-group col-md-4">
-                        <label for="noOfDays">Number of Days:</label>
-                        <input type="number" class="form-control" id="noOfDays" name="noOfDays[]" required readonly>
+    <div class="card-body">
+        <!-- Form for Packages Management -->
+        <form action="" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div>
+                <div class="firstRow">
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <label for="noOfDays">Number of Days:</label>
+                            <input type="number" class="form-control" id="noOfDays" name="noOfDays[]" required readonly>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="hotelName">Hotel Name:</label>
+                            <input type="text" class="form-control" id="hotelName" name="hotelName[]" required>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="meals">Meals:</label>
+                            <br>
+                            <div class="checkbox-group">
+                                <input class="custom-checkbox" type="checkbox" id="breakfastCheckbox" name="meals[]"
+                                    value="B">
+                                <label class="custom-label" for="breakfastCheckbox">Breakfast</label>
+                            </div>
+                            <div class="checkbox-group">
+                                <input class="custom-checkbox" type="checkbox" id="lunchCheckbox" name="meals[]"
+                                    value="L">
+                                <label class="custom-label" for="lunchCheckbox">Lunch</label>
+                            </div>
+                            <div class="checkbox-group">
+                                <input class="custom-checkbox" type="checkbox" id="dinnerCheckbox" name="meals[]"
+                                    value="D">
+                                <label class="custom-label" for="dinnerCheckbox">Dinner</label>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group col-md-4">
-                        <label for="hotelName">Hotel Name:</label>
-                        <input type="text" class="form-control" id="hotelName" name="hotelName[]" required>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="meals">Meals:</label>
-                        <br>
-                        <div class="checkbox-group">
-                            <input class="custom-checkbox" type="checkbox" id="breakfastCheckbox" name="meals[]"
-                                value="B">
-                            <label class="custom-label" for="breakfastCheckbox">Breakfast</label>
-                        </div>
-                        <div class="checkbox-group">
-                            <input class="custom-checkbox" type="checkbox" id="lunchCheckbox" name="meals[]" value="L">
-                            <label class="custom-label" for="lunchCheckbox">Lunch</label>
-                        </div>
-                        <div class="checkbox-group">
-                            <input class="custom-checkbox" type="checkbox" id="dinnerCheckbox" name="meals[]" value="D">
-                            <label class="custom-label" for="dinnerCheckbox">Dinner</label>
-                        </div>
+                    <div class="form-group">
+                        <label for="information">Information:</label>
+                        <textarea class="form-control long-textarea" id="information" name="information[]"
+                            required></textarea>
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="information">Information:</label>
-                    <textarea class="form-control long-textarea" id="information" name="information[]" required></textarea>
+                <div>
+                    <!-- <button type="submit" class="btn btn-primary">Submit</button> -->
+                    <button class="btn btn-warning addField">Add More</button>
                 </div>
             </div>
+         </form>
+
+    </div>
+    <button class="btn btn-success nextButton" onclick="topFunction()">Next</button>
+
+</div>
+
+</div>
+</div>
+</div>
+
+
+<div id="step2" style="display: none;">
+    <div class="row"> 
+        <div class="col-md-8"> 
+            <div class="card"> <div class="card-header"> 2</div>
+
+    <div class="card-body">
+        <!-- Form for Packages Management -->
+        <!-- <form action="" method="POST" enctype="multipart/form-data">
+            @csrf
             <div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-                <button class="btn btn-warning addField">Add More</button>
-    </form>
+                <div class="firstRow">
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <label for="noOfDays">Number of Days:</label>
+                            <input type="number" class="form-control" id="noOfDays" name="noOfDays[]" required readonly>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="hotelName">Hotel Name:</label>
+                            <input type="text" class="form-control" id="hotelName" name="hotelName[]" required>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="meals">Meals:</label>
+                            <br>
+                            <div class="checkbox-group">
+                                <input class="custom-checkbox" type="checkbox" id="breakfastCheckbox" name="meals[]"
+                                    value="B">
+                                <label class="custom-label" for="breakfastCheckbox">Breakfast</label>
+                            </div>
+                            <div class="checkbox-group">
+                                <input class="custom-checkbox" type="checkbox" id="lunchCheckbox" name="meals[]"
+                                    value="L">
+                                <label class="custom-label" for="lunchCheckbox">Lunch</label>
+                            </div>
+                            <div class="checkbox-group">
+                                <input class="custom-checkbox" type="checkbox" id="dinnerCheckbox" name="meals[]"
+                                    value="D">
+                                <label class="custom-label" for="dinnerCheckbox">Dinner</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="information">Information:</label>
+                        <textarea class="form-control long-textarea" id="information" name="information[]"
+                            required></textarea>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button class="btn btn-warning addField">Add More</button>
+                </div>
+            </div>
+         </form> -->
+
+    </div>
+    <button class="btn btn-primary previousButton">Back</button>
+    <button class="btn btn-success nextButton">Next</button>
 </div>
 
 </div>
 </div>
 </div>
 </div>
+
+
 <script>
+    function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
     $(document).ready(function () {
         // Initialize the number of days counter
         var numOfDays = 1;
@@ -113,6 +194,52 @@
             numOfDays--;
         });
     });
+
+
+    //multi-step form
+    $(document).ready(function () {
+        var currentStep = 1;
+
+
+        $('.nextButton').click(function () {
+            // Validate current step fields here
+            // If validation passes, move to the next step
+            // ...
+
+            // Move to the next step
+
+            $('#' + 'step' + currentStep).hide();
+            document.documentElement.scrollTop = 0;
+            currentStep++;
+            $('#' + 'step' + currentStep).show();
+
+            // Show the previous button from the second step onwards
+            if (currentStep > 1) {
+                $('#' + 'step' + currentStep + ' .previousButton').show();
+            }
+
+
+
+        });
+
+        $('.previousButton').click(function () {
+            // Move to the previous step
+            $('#' + 'step' + currentStep).hide();
+            currentStep--;
+            $('#' + 'step' + currentStep).show();
+
+            // Hide the previous button in the first step
+            if (currentStep === 1) {
+                $('#' + 'step' + currentStep + ' .previousButton').hide();
+            }
+        });
+
+        $('.submitButton').click(function () {
+            // Handle form submission here
+            // ...
+        });
+    });
+
 
 </script>
 
