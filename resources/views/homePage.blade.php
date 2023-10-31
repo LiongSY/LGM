0bb9c0e0f4897dd25e3bcfece8862b34e4e81480
CHINPL <pohlianchin0107@gmail.com>
2023-11-01 00:21:25 +0800

    1/11 until our services
    Changes all nav until our services part

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1,74 +1,197 @@
-@extends('layouts/customer/app') 
+
+@extends('layouts.customers.app')
 
 @section('content')
-    <div class="media-icons">
-        <a href=""><i class="uil uil-facebook-f"></i></a>
-        <a href=""><i class="uil uil-instagram"></i></a>
-        <a href=""><i class="uil uil-whatsapp"></i></a>
+
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel" >
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/autumn.jpeg" class="d-block w-100" alt="...">
     </div>
-
-    <div class="swiper bg-slider">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <img src="{{ asset('images/spring.jpg') }}" alt="">
-                   <!-- <div class="text-content">
-                    <h2 class="title">Spring <span>Season</span></h2>
-                    <p>Spring is the time when the world awakens, and your journey should be no different. Pack your bags, set forth on a blooming adventure, and experience the splendor of this season's travel possibilities.</p>
-                    <button class="choose-btn">Travel Now <i class="uil uil-arrow-right"></i></button>
-                </div> -->
-            </div>
-
-            <div class="swiper-slide dark-layer">
-                <img src="{{ asset('images/summer.jpeg') }}" alt="">
-                <!-- <div class="text-content">
-                    <h2 class="title">Summer <span>Season</span></h2>
-                    <p>Embrace the warmth of the sun, the cool breeze, and the endless adventures. Summer is the perfect time to explore new places, create unforgettable memories, and bask in the beauty of the world.</p>
-                    <button class="choose-btn"> Travel Now <i class="uil uil-arrow-right"></i></button>
-                </div> -->
-            </div>
-
-            <div class="swiper-slide dark-layer">
-                <img src="{{ asset('images/autumn.jpeg') }}" alt="">
-                <!-- <div class="text-content">
-                    <h2 class="title">Autumn <span>Season</span></h2>
-                    <p>This is the season for capturing photographs that encapsulate the essence of autumn's splendor, for sharing stories around bonfires, and for exploring the cozy corners of the world. Let the beauty of autumn be your guide to memorable journeys.</p>
-                    <button class="choose-btn"> Travel Now <i class="uil uil-arrow-right"></i></button>
-                </div> -->
-            </div>
-
-            <div class="swiper-slide">
-                <img src="{{ asset('images/winter.jpeg') }}" alt="">
-                <!-- <div class="text-content">
-                    <h2 class="title">Winter <span>Season</span></h2>
-                    <p>Winter travel is a symphony of cozy moments, frosty landscapes, and the magic of the season.  Let's wander where the snowflakes kiss the earth.</p>
-                    <button class="choose-btn"> Travel Now <i class="uil uil-arrow-right"></i></button>
-                </div> -->
-            </div>
-        </div>
+    <div class="carousel-item">
+      <img src="images/winter.jpeg" class="d-block w-100" alt="...">
     </div>
-
-    <div class="bg-slider-thumbs">
-        <div class="swiper-wrapper thumbs-container">
-            <img src="{{ asset('images/spring.jpg') }}" class="swiper-slide" alt="">
-            <img src="{{ asset('images/summer.jpeg') }}" class="swiper-slide" alt="">
-            <img src="{{ asset('images/autumn.jpeg') }}" class="swiper-slide" alt="">
-            <img src="{{ asset('images/winter.jpeg') }}" class="swiper-slide" alt="">
-        </div>
+    <div class="carousel-item">
+      <img src="images/summer.jpeg" class="d-block w-100" alt="...">
     </div>
-
-    <div class="trip_bx">
-        <div class="search_bx">
-            <div class="card">
-                <h3>Location <i class="bi bi-geo-alt"></i></h3>
-                <input type="text" placeholder="Enter your destination">
-            </div>
-            <div class="card">
-                <h3>Date <i class="bi bi-caret-down-fill"></i></h3>
-                <input type="date" >
-            </div>
-          <input type="button" value="Explore Now">
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 
+
+<!-- Search -->
+<div class="container mt-3" >
+    <form>
+        <div class="row justify-content-center" style="border:1px solid black; border-radius:20px;">
+            <div class="col-md-3">
+                <div class="card" style="border:none;">
+                    <!-- <h3 class="text-center">Location <i class="bi bi-geo-alt"></i></h3> -->
+                    <input class="form-control" type="text" placeholder="Enter your destination">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="border:none;">
+                    <!-- <h3 class="text-center">Date <i class="bi bi-caret-down-fill"></i></h3> -->
+                    <input class="form-control" type="date">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="border:none; border-radius:10px;">
+                    <button class="btn btn-primary btn-block">Explore Now</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+    <div id="carouselExampleControls" class="carousel carousel-dark slide mb-4"> <!-- Added "mb-4" class for margin at the bottom -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <h4 class="text-center mt-5"><b>Trending Packages<b></h4> <!-- Increased the top margin for additional space -->
+                <div class="card-wrapper container-sm d-flex justify-content-around">
+                    <div class="card" style="width: 20rem; border:none;">
+                        <img src="images/tour1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold">Korea Trip 7D6N</h5>
+                            <p class="card-text">RM3855.00 ++</p>
+                            <p class="font-weight-normal">Korea: From stunning landscapes to rich culture, Korea offers a world of exploration waiting for you to discover.</p>
+                            <a href="#" class="btn btn-primary mt-3">Explore Now</a>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 20rem; border:none;">
+                        <img src="images/tour2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold">Japan Trip 6D5N</h5>
+                            <p class="card-text">RM4788.00 ++</p>
+                            <p class="font-weight-normal">Japan: Go japan go travel good good good</p>
+                            <a href="#" class="btn btn-primary mt-3">Explore Now</a>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 20rem; border:none;">
+                        <img src="images/tour3.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold">Europe Trip 5D4N</h5>
+                            <p class="card-text">RM8388 ++</p>
+                            <p class="font-weight-normal">Europe: A tapestry of history, art, and culture that's yours to explore. Journey through picturesque landscapes and iconic cities on your European adventure.</p>
+                            <a href="#" class="btn btn-primary mt-3">Explore Now</a>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+            </div>
+            
+           
+<div class="container-services text-center">
+    <div class="row justify-content-center align-items-start mb-5">
+        <div class="col-lg-6 custom-margin">
+            <h3 class="section-title mb-3">Our Services</h3>
+            <p>Unlocking Unforgettable Journeys: Our Services, Your Adventures.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <section>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="icon-wrapper">
+                        <i class="fas fa-hammer"></i>
+                    </div>
+                    <h3 class="card-title">Service Heading</h3>
+                    <p class="service">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="icon-wrapper">
+                        <i class="fas fa-brush"></i>
+                    </div>
+                    <h3 class="card-title">Service Heading</h3>
+                    <p class="service">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="icon-wrapper">
+                        <i class="fas fa-wrench"></i>
+                    </div>
+                    <h3 class="card-title">Service Heading</h3>
+                    <p class="service">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="icon-wrapper">
+                        <i class="fas fa-truck-pickup"></i>
+                    </div>
+                    <h3 class="card-title">Service Heading</h3>
+                    <p class="service">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="icon-wrapper">
+                        <i class="fas fa-broom"></i>
+                    </div>
+                    <h3 class="card-title">Service Heading</h3>
+                    <p class="service">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="icon-wrapper">
+                        <i class="fas fa-plug"></i>
+                    </div>
+                    <h3 class="card-title">Service Heading</h3>
+                    <p class="service">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consequatur necessitatibus eaque.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+
+            <!-- Add more carousel items here -->
+      
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 
 @endsection
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
---
+++ resources/views/layouts/customers/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LGM Tour & Travel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-transparent fixed-top" >
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="images/LGM.png" alt="Logo"></a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Packages</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+@yield('content')
+
+</body>
+</html>
+
